28011e494883a5e073cb2b8a50aaa7a6a0c8c8dc
Jacob Kurstedt <vtkurt13@vt.edu>
2020-08-25 15:09:05 -0400

    make it better

--- client/src/components/ButtonCard.js
+++ client/src/components/ButtonCard.js
@@ -102,8 +102,9 @@ export default class ButtonCard extends Component {
                     </Modal.Header>
                     <ModalBody className='print-container'>
                         <div ref={el => (this.componentRef = el)}>
+                            <br></br>
                             <h4>Description: {this.state.description}</h4>                  
-                            <img src={this.state.imageUrl} alt='loading' style={{width: '48em', height:'45em'}}/>
+                            <img src={this.state.imageUrl} alt='loading' style={{width: '100%', height:'800%'}}/>
                         </div>
                     </ModalBody>
                  </Modal>
